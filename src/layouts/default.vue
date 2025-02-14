00522fef6d43ad2b82c075af403ab777cbe3fcb5
fung <fung.8021@gmail.com>
2025-02-14 16:36:14 +0800

    2/14 上傳

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
   <v-app-bar>
     <v-container class="d-flex align-center">
-      <v-btn to="/" :active="false">放 logo 圖回 welcome</v-btn>
+      <router-link to="/" :active="false" >
+        <img src="/antlogo.ico" alt="Logo" style="height: 45px; margin-top: 5px;">
+      </router-link>
+      <router-link to="/about" :active="false" style="color: aliceblue; margin-left: 10px;">
+        <v-btn text>{{ '關於' }}</v-btn>
+      </router-link>
+      <v-menu v-if="user.isLoggedIn || !user.isLoggedIn" offset-y>
+        <template #activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="nav in navs" v-if="nav.show" :key="nav.to" @click="navigate(nav.to)">
+            <v-list-item-title>{{ nav.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-spacer />
+        <v-btn v-if="user.isLoggedIn" icon="mdi-chat-processing"></v-btn>
         <template v-for="nav of navs" :key="nav.to">
           <v-btn v-if="nav.show" :to="nav.to" :prepend-icon="nav.icon">
             {{ nav.text }}
@@ -35,14 +53,16 @@ const router = useRouter()
 // 導覽列項目
 const navs = computed(() => {
   return [
-    { icon: 'mdi-chat-processing', show: user.isLoggedIn }, //訊息通知 => 使用者登入時
     { to: '/register', text: '註冊', icon: 'mdi-account-plus' , show: !user.isLoggedIn },
     { to: '/login', text: '登入', icon: 'mdi-account-arrow-left' , show: !user.isLoggedIn  },
     { to: '/userPage', text: '個人頁面', icon: 'mdi-account', show: user.isLoggedIn },
-    { to: '/about', text: '關於', show: true },
     { to: '/admin', text: '後台管理', icon: 'mdi-cog', show: user.isLoggedIn && user.isAdmin },
   ]
 })
+
+const navigate = (to) => {
+  router.push(to)
+}
 
 // 登出
 const logout = async () => {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,31 +4,42 @@
       <v-col cols="12">
         <v-text-field v-model="search" prepend-inner-icon="mdi-magnify"></v-text-field>
       </v-col>
+      <v-col cols="12">
+        <v-btn v-for="cat in categories" :key="cat" class="mx-2" :class="{ 'v-btn--active': category === cat }" @click="category = cat">
+          {{ cat }}
+        </v-btn>
+      </v-col>
       <v-col v-for="post of filteredPosts" :key="post._id" cols="12" md="6" lg="3" >
         <post-cart v-bind="post"></post-cart>
       </v-col>
-      <v-col cols="12">
-        <v-pagination v-model="currentPage" :length="totalPage"></v-pagination>
-      </v-col>
+      <!-- <v-col cols="12"> -->
+        <!-- <v-pagination v-model="currentPage" :length="totalPage"></v-pagination> -->
+      <!-- </v-col> -->
     </v-row>
+    <div ref="loadMoreTrigger" class="load-more-trigger"></div>
   </v-container>
 </template>
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
 import { useAxios } from '@/composables/axios'
 import PostCart from '@/components/PostCart.vue'
 
 const { api } = useAxios()
 
+// ----------------------------分頁----------------------------------
 // FIXME 改成不分頁，一直往下延伸
-// 分頁
-const ITEMS_PER_PAGE = 4 //一頁4個
-const currentPage = ref(1) //目前在第一頁
+// const ITEMS_PER_PAGE = 4 //一頁4個
+// const currentPage = ref(1) //目前在第一頁
 // 例如：現在有5個，一頁2個 => 5/2 => 2.5 => 無條件進位到第3頁
-const totalPage = computed(() => Math.ceil(posts.value.length / ITEMS_PER_PAGE)) //總共有幾頁
+// const totalPage = computed(() => Math.ceil(posts.value.length / ITEMS_PER_PAGE)) //總共有幾頁
 
 const posts = ref([])
 const search = ref('')
+const category = ref('全部') // 分類變數，預設為'全部'
+const categories = ['全部','紀念繪畫', '回憶拼貼', '故事攝影', '物品改造', '其他'] // 分類選項
+const ITEMS_PER_LOAD = 4 // 每次加載的項目數量
+let loading = false // 加載狀態
+
 const filteredPosts = computed(() => {
   return posts.value
   .filter(post => post.title.toLowerCase().includes(search.value.toLowerCase()))
@@ -38,23 +49,58 @@ const filteredPosts = computed(() => {
     // 第 3 頁 = 4 ~ 5
     // .slice(開始索引，結束索引)
     // 不包含結束索引
-    .slice((currentPage.value - 1) * ITEMS_PER_PAGE, currentPage.value * ITEMS_PER_PAGE)
+    // .slice((currentPage.value - 1) * ITEMS_PER_PAGE, currentPage.value * ITEMS_PER_PAGE)
+    .filter(post => category.value === '全部' || post.category === category.value) // 根據分類過濾
 })
 
 const getPosts = async () => {
+  if (loading) return
+  loading = true
   try {
-    const { data } = await api.get('/post')
+    const { data } = await api.get('/post', {
+      params: {
+        offset: posts.value.length,
+        limit: ITEMS_PER_LOAD
+      }
+    })
     posts.value.push(...data.result)
   } catch (error) {
     console.log(error)
   }
+  loading = false
 }
-getPosts()
+
+const handleScroll = () => {
+  // 只有當 category 的值為 '全部' 時才會觸發 getPosts 函數來加載更多資料。
+  if (category.value !== '全部') return
+  const trigger = loadMoreTrigger.value
+  if (trigger && trigger.getBoundingClientRect().top <= window.innerHeight) {
+    getPosts()
+  }
+}
+
+const loadMoreTrigger = ref(null)
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+  getPosts() // 初始加載
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
+
+<style>
+.load-more-trigger {
+  height: 1px;
+}
+</style>
 
 <route lang="yaml">
   meta:
     login: false
     admin: false
     title: '首頁'
-  </route>
+</route>
+
